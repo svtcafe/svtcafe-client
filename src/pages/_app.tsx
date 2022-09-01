7d7a444c9e27e09e01971379f907a882dc4dcf28
khsily <khsilyqwerty@gmail.com>
2022-09-01 22:53:39 +0900

    init: 초기 설정

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app';
+
+import '@/styles/reset.css';
+import '@/styles/font.css';
+import '@/styles/global.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return <Container>home</Container>;
+};
+
+export default Home;
+
+const Container = styled.div`
+  font-size: 2rem;
+`;

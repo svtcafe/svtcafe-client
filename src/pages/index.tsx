aaf65c14882dc053c654fc4c3d7a7323d3d3fe85
eunjinfizz <eunjinfizz@gmail.com>
2022-09-03 15:30:24 +0900

    feat: Button 컴포넌트 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,40 @@
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 
+import { Button } from '@/components';
+
 const Home: NextPage = () => {
-  return <Container>home</Container>;
+  return (
+    <Container>
+      home
+      <TestZone>
+        <Button size='xs' color='light' label='등록' />
+        <Button size='xs' color='primary' label='등록' />
+        <Button size='xs' color='dark' label='등록' />
+        <Button size='xs' color='light' outlined label='등록' />
+      </TestZone>
+      <TestZone>
+        <Button label='Sign In' outlined color='light' />
+        <Button label='Sign Up' color='primary' />
+      </TestZone>
+      <TestZone>
+        <Button label='진행중인 이벤트' color='primary' shadow />
+        <Button label='종료된 이벤트' color='light' outlined shadow />
+      </TestZone>
+      <TestZone style={{ width: 300 }}>
+        <Button label='SIGN UP' color='primary' fullWidth />
+      </TestZone>
+    </Container>
+  );
 };
 
 export default Home;
 
 const Container = styled.div`
   font-size: 2rem;
+`;
+
+const TestZone = styled.div`
+  display: flex;
+  margin-bottom: 20px;
 `;

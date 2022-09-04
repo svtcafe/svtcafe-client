c66e034e8481d2a8f913cae3216c4a925451ed90
khsily <khsilyqwerty@gmail.com>
2022-09-04 21:09:47 +0900

    feat: layout fullWidth 옵션 추가

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -4,5 +4,6 @@ import type { ReactElement, ReactNode } from 'react';
 declare global {
   type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
     hideFooter?: boolean;
+    fullContentWidth?: boolean;
   };
 }
